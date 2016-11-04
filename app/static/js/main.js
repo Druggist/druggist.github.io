c72bb7b1648c42afd1653b9cded76340eb5842c7
Druggist <m.najdora@gmail.com>
2016-11-04 01:13:58 +0100

    Sections & scroll animation

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,3 +1,28 @@
+$(function() {
+	$.scrollify({
+		section : ".section",
+		sectionName : "section-name",
+		interstitialSection : "",
+		easing: "easeOutExpo",
+		scrollSpeed: 1200,
+		offset : 0,
+		scrollbars: true,
+		standardScrollElements: "",
+		setHeights: false,
+		overflowScroll: true,
+	//	before:function() {},
+	//	after:function() {},
+	//	afterResize:function() {},
+	//	afterRender:function() {}
+	});
+});
+
 $(document).ready(function(){
 	$(".button-collapse").sideNav();
 }); 
+
+
+$('a[href="#about_me"]').click(function(e){
+	e.preventDefault();
+	$.scrollify.move("#about_me");
+});

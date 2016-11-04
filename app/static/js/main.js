423c18c5d7e586fe0a365c88ae1582c26d603325
Druggist <m.najdora@gmail.com>
2016-11-04 15:25:24 +0100

    Vertical menu & fixes

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -4,16 +4,25 @@ $(function() {
 		sectionName : "section-name",
 		interstitialSection : "",
 		easing: "easeOutExpo",
-		scrollSpeed: 1200,
+		scrollSpeed: 900,
 		offset : 0,
 		scrollbars: true,
 		standardScrollElements: "",
 		setHeights: false,
 		overflowScroll: true,
-	//	before:function() {},
-	//	after:function() {},
-	//	afterResize:function() {},
-	//	afterRender:function() {}
+		before:function() {
+			$currSection = $.scrollify.current();
+			$href = $currSection[0].id.replace('_section','');
+			menuClasses($href);
+		},
+		after:function() { 
+		},
+		afterResize:function() {},
+		afterRender:function() {
+			$currSection = $.scrollify.current();
+			$href = $currSection[0].id.replace('_section','');
+			menuClasses($href);
+		}
 	});
 	appendEmail();
 });
@@ -22,15 +31,77 @@ $(document).ready(function(){
 	$(".button-collapse").sideNav();
 }); 
 
+$('a[href="#main"]').click(function(e){
+	e.preventDefault();
+	$.scrollify.move("#main");
+});
 
 $('a[href="#about_me"]').click(function(e){
 	e.preventDefault();
 	$.scrollify.move("#about_me");
 });
 
+$('a[href="#websites"]').click(function(e){
+	e.preventDefault();
+	$.scrollify.move("#websites");
+});
+
+$('a[href="#games"]').click(function(e){
+	e.preventDefault();
+	$.scrollify.move("#games");
+});
+
+$('a[href="#apps"]').click(function(e){
+	e.preventDefault();
+	$.scrollify.move("#apps");
+});
+
+$('a[href="#support_me"]').click(function(e){
+	e.preventDefault();
+	$.scrollify.move("#support_me");
+});
+
+$('a[href="#contact"]').click(function(e){
+	e.preventDefault();
+	$.scrollify.move("#contact");
+});
+
+$('#next').click(function(e){
+	e.preventDefault();
+	$.scrollify.next();
+});
+
+$('#previous').click(function(e){
+	e.preventDefault();
+	$.scrollify.previous();
+});
 
 function appendEmail(){
 	var user = 'm.najdora',
     	domain = 'gmail.com';
     $('.email').append(user + '@' + domain);
+}
+
+function menuClasses($href){
+	if($href == 'contact') $("#next").addClass('disabled');
+	else $("#next").removeClass('disabled');
+
+	if($href == 'main') $(".vertical-bullet-menu").addClass('close');
+	else {
+		if($('.vertical-bullet-menu').hasClass("close")) 
+			setTimeout(function(){
+				$(".vertical-bullet-menu").removeClass('close')
+				setTimeout(function(){
+					$('a[href="#'+$href+'"]').removeClass('expanded');
+				}, 1500);
+			}, 900);
+		else {	
+			setTimeout(function(){
+				$('a[href="#'+$href+'"]').removeClass('expanded');
+			}, 1500);
+		}
+	}
+
+	$("a").removeClass("active expanded")
+	$('a[href="#'+$href+'"]').addClass("active expanded");
 }

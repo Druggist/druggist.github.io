0e81b0b455f07927402505344dcc70687802d043
Druggist <m.najdora@gmail.com>
2017-03-24 18:11:26 +0100

    Mobile fixes

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,90 +1,99 @@
-var tiles;
+var tiles,
+	isMobile = false; //initiate as false
+
 $(function() {
-	$.scrollify({
-		section : ".section",
-		sectionName : "section-name",
-		interstitialSection : "",
-		easing: "easeOutExpo",
-		scrollSpeed: 900,
-		offset : 0,
-		scrollbars: true,
-		standardScrollElements: "",
-		setHeights: false,
-		overflowScroll: true,
-		before:function() {
-			$currSection = $.scrollify.current();
-			$href = $currSection[0].id.replace('_section','');
-			menuClasses($href);
-		},
-		after:function() { 
-		},
-		afterResize:function() {},
-		afterRender:function() {
-			$.scrollify.update();
-			$currSection = $.scrollify.current();
-			$href = $currSection[0].id.replace('_section','');
-			menuClasses($href);
-		}
-	});
+// device detection
+if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
+
+    if(!isMobile){
+		$.scrollify({
+			section : ".section",
+			sectionName : "section-name",
+			interstitialSection : "",
+			easing: "easeOutExpo",
+			scrollSpeed: 900,
+			offset : 0,
+			scrollbars: true,
+			standardScrollElements: "",
+			setHeights: true,
+			overflowScroll: true,
+			updateHash: true,
+			touchScroll:true,
+			before:function() {
+				$currSection = $.scrollify.current();
+				$href = $currSection[0].id.replace('_section','');
+				menuClasses($href);
+			},
+			after:function() { 
+			},
+			afterResize:function() {},
+			afterRender:function() {
+				$.scrollify.update();
+				$currSection = $.scrollify.current();
+				$href = $currSection[0].id.replace('_section','');
+				menuClasses($href);
+			}
+		});
+	}
 	loadData();
 	appendEmail();
 });
 
 $(document).ready(function(){
 	$(".button-collapse").sideNav();
+	$('.scrollspy').scrollSpy({
+		scrollOffset: 0
+	});
 	$('.modal').modal({
 		starting_top: '0',
       	ending_top: '0',
       	opacity: .9,
       	complete: function() {
-      		$.scrollify.enable();
+      		 if(!isMobile) $.scrollify.enable();
       	}
 	});
 }); 
 
-$('a[href="#main"]').click(function(e){
-	e.preventDefault();
-	$.scrollify.move("#main");
+$(window).scroll(function(){
+	if(isMobile){
+		$href = $('a.active[href$="_section"]').attr('href');
+		$href = $href.replace('#','');
+		$href = $href.replace('_section','');
+		$(".nav-trigger").attr('class', 'nav-trigger');
+		$(".nav-trigger").addClass($href);
+	}
 });
 
-$('a[href="#about_me"]').click(function(e){
-	e.preventDefault();
-	$.scrollify.move("#about_me");
+$('a[href^="#"]').click(function(e){
+	 if(!isMobile){
+		e.preventDefault();
+		$.scrollify.move($(this).attr('href'));
+	}
 });
 
-$('a[href="#websites"]').click(function(e){
-	e.preventDefault();
-	$.scrollify.move("#websites");
-});
-
-$('a[href="#games"]').click(function(e){
-	e.preventDefault();
-	$.scrollify.move("#games");
-});
-
-$('a[href="#apps"]').click(function(e){
-	e.preventDefault();
-	$.scrollify.move("#apps");
-});
-
-$('a[href="#support_me"]').click(function(e){
-	e.preventDefault();
-	$.scrollify.move("#support_me");
-});
-
-$('a[href="#contact"]').click(function(e){
-	e.preventDefault();
-	$.scrollify.move("#contact");
+$('a[href$="_section"]').click(function(e){
+	if(isMobile){
+		$href = $(this).attr('href');
+		$href = $href.replace('#','');
+		$href = $href.replace('_section','');
+		$(".nav-trigger").attr('class', 'nav-trigger');
+		$(".nav-trigger").addClass($href);
+	}
 });
 
 $('#next').click(function(e){
-	e.preventDefault();
-	$.scrollify.next();
+	if(!isMobile){
+		e.preventDefault();
+		$.scrollify.next();
+	}
 });
 
 $('#previous').click(function(e){
-	e.preventDefault();
-	$.scrollify.previous();
+	if(!isMobile){
+		e.preventDefault();
+		$.scrollify.previous();
+	}
 });
 
 $('.close').click(function(e){
@@ -101,7 +110,7 @@ $('.modal-content').click(function(event){
 });
 
 $('body').on('click', '.modal-trigger', function() {
-	$.scrollify.disable();
+	if(!isMobile) $.scrollify.disable();
 	var data = $(this).attr("data-id").split("_");
 	$(".close-wrapper").attr('class', 'close-wrapper');
 	$(".close-wrapper").addClass(data[0]);

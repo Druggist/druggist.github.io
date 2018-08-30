546c4777e5b7572b6311d26187693053961a00c7
Druggist <m.najdora@gmail.com>
2018-08-30 08:57:53 +0200

    Color changes

--- src/Components/PersonalInfo/PersonalInfo.js
+++ src/Components/PersonalInfo/PersonalInfo.js
@@ -11,6 +11,14 @@ class PersonalInfo extends React.Component {
 					<Col xs={12}>
 						<img src={DruggistImg} alt="druggist"/>
 						<h1>sir Druggist</h1>
+						<a className="card" href="#">
+							<i className="far fa-envelope"></i>
+							<span id="email"></span>
+						</a>
+						<a className="card" href="#">
+							<i className="fab fa-twitter"></i>
+							<span>@sirDruggist</span>
+						</a>
 					</Col>
 				</Row>
 			</Grid>
